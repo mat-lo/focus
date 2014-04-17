5a8349b54f5c8185d2737619ee9dfeda519878e0
mat-lo <matteo.loglio@gmail.com>
2014-04-17 22:47:55 +0100

    + array of websites

--- background.js
+++ background.js
@@ -1,12 +1,25 @@
+var bannedURLs = [
+	"facebook.com",
+	"twitter.com",
+	"reddit.com",
+	"4chan.org"
+]
+
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {   	
-  	if (tab.url.indexOf("facebook.com") > -1) {
-    // ... show the page action.
-    chrome.pageAction.show(tabId);    
-    chrome.tabs.remove(tabId);
-  	}  
-
+	for (var i = 0; i < bannedURLs.length; i++) {
+		if (tab.url.indexOf(bannedURLs[i]) > -1) {
+	    // ... show the page action.
+	    chrome.pageAction.show(tabId);    
+	    chrome.tabs.remove(tabId);
+	  	}  
+	}  	
 };
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+
 
 // Listen for any changes to the URL of any tab.
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
